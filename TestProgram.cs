e78d351a459dc39b1e90e4c70218f76f7e4a682f
patrickyuibm <150204185+patrickyuibm@users.noreply.github.com>
2023-11-08 14:32:30 -0800

    Update TestProgram.cs

--- TestProgram.cs
+++ TestProgram.cs
@@ -14,10 +14,17 @@ using IBM.Data.DB2Types;
 using System.Text;
 
 public void callSP(DB2Connection conn) {
+  DB2DataReader db2reader = null;
+  DB2Command db2cmd = null;
+  DB2Transaction trans = null;
+
   String procName = "";
   String cmdString = "CALL " + procName + "(?,?)";
+
+  trans = conn.BeginTransaction();
   DB2Command db2cmd = conn.createCommand();
   db2cmd.CommandText = cmdString;
+  
   Console.WriteLine(db2cmd.CommandText);
   DB2DataReader db2reader = db2cmd.ExecuteReader();
   Console.WriteLine("===========================================");
@@ -26,5 +33,6 @@ public void callSP(DB2Connection conn) {
   
   db2reader.Close();
   db2cmd.Dispose();
+  trans.Dispose();
   conn.Close();  
 }
