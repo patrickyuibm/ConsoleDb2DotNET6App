551c32b21bcbd65f6b72ae141929fa67f8914343
patrickyuibm <150204185+patrickyuibm@users.noreply.github.com>
2023-11-08 12:53:00 -0800

    Update TestProgram.cs

--- TestProgram.cs
+++ TestProgram.cs
@@ -1,2 +1,30 @@
+using System;
+using System.Data;
+using System.Threading;
+using System.IO;
+using IBM.Data.DB2;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Dynamic;
+using System.Xml;
+using IBM.Data.DB2Types;
+using System.Text;
 
-Console.WriteLine('hello')
+public void callSP(DB2Connection conn) {
+  String procName = "";
+  String cmdString = "CALL " + procName + "(?,?)";
+  DB2Command db2cmd = conn.createCommand();
+  db2cmd.CommandText = cmdString;
+  Console.WriteLine(db2cmd.CommandText);
+  DB2DataReader db2reader = db2cmd.ExecuteReader();
+  Console.WriteLine("===========================================");
+  
+  //print the output here
+  
+  db2reader.Close();
+  db2cmd.Dispose();
+  conn.Close();  
+}
