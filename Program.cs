89744a06152a3553039d9d36fa87d42a48434014
patrickyuibm <150204185+patrickyuibm@users.noreply.github.com>
2023-11-09 10:48:36 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,6 @@
-﻿
-// See https://aka.ms/new-console-template for more information
+﻿// See https://aka.ms/new-console-template for more information
 
 using IBM.Data.Db2;
-
-Console.WriteLine("Using DB2 .NET6 provider");
-Console.WriteLine("test edit");
 
 string uid = Environment.GetEnvironmentVariable("uid");
 string pwd = Environment.GetEnvironmentVariable("pwd");
@@ -13,14 +9,30 @@ string db = Environment.GetEnvironmentVariable("db");
 
 //Connection String
 string connString = "uid=" + uid + ";pwd=" + pwd + ";server=" + server + ";database=" + db;
-
-DB2Connection con = new DB2Connection(connString);
-con.Open();
+DB2Connection conn = new DB2Connection(connString);
+conn.Open();
 Console.WriteLine("Connection Opened successfully");
 
+DB2Transaction trans = conn.BeginTransaction();
+DB2Command cmd = conn.CreateCommand();
+String procName = "DB2ADM.INSERT_AND_SELECT_TB2";
+String procCall = "CALL " + procName + " (@param1, @param2)";
+cmd.Transaction = trans;
+cmd.CommandType = CommandType.Text;
+cmd.CommandText = procCall;
+
+// Register input-output and output parameters for the DB2Command
+cmd.Parameters.Add( new DB2Parameter("@param1", 5);
+cmd.Parameters.Add( new DB2Parameter("@param2", 6);
+
+// Call the stored procedure
+Console.WriteLine("  Call stored procedure named " + procName);
+cmd.ExecuteReader();
+                   
+/*
+//Old code
 //Run a SQL query
 Console.Read();
-
 string mySelectQuery = "SELECT * FROM SYSIBM.SYSDUMMY1";
           
 DB2Command myCommand = new DB2Command(mySelectQuery, con);
@@ -42,9 +54,4 @@ finally
     Console.WriteLine("Connection Closed");  
 }
 
-
-/*
-Old code
-con.Close();
-Console.WriteLine("Connection Closed");
 */
