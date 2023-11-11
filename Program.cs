c6e538a24b3b0a0f67fa0b6dc6071bd8c176242d
patrickyuibm <150204185+patrickyuibm@users.noreply.github.com>
2023-11-10 17:12:10 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+//***************************** OPEN THE CONNECTION *****************************
 using IBM.Data.Db2;
 
 string uid = Environment.GetEnvironmentVariable("uid");
@@ -12,25 +13,44 @@ string connString = "uid=" + uid + ";pwd=" + pwd + ";server=" + server + ";datab
 DB2Connection conn = new DB2Connection(connString);
 conn.Open();
 Console.WriteLine("Connection Opened successfully");
+//*******************************************************************************
 
-//***************************** STORED PROCEDURE *****************************
 
-//Set up a stored procedure
-DB2Transaction trans = conn.BeginTransaction();
-DB2Command cmd = conn.CreateCommand();
-String procName = "DB2ADM.INSERT_AND_SELECT_TB2";
-String procCall = "CALL " + procName + " (@param1, @param2)";
-cmd.Transaction = trans;
-//cmd.CommandType = CommandType.Text;
-cmd.CommandText = procCall;
+//***************************** METHODS *****************************
+//Method to run stored procedure
+/*
+Note: currently, since the parameters for each SP are different, one method to run any SP is nearly impossible. We have to know what 
+SP we want to run the code it in first. 
+*/
+void runSP(String spname = 'DB2ADM.INSERT_AND_SELECT_TB2', DB2Connection conn) {
+  //Set up a stored procedure
+  DB2Transaction trans = conn.BeginTransaction();
+  DB2Command cmd = conn.CreateCommand();
+  String procCall = "CALL " + spname + " (@param1, @param2)";
+  cmd.Transaction = trans;
+  cmd.CommandText = procCall;
+  // Register input-output and output parameters for the DB2Command
+  cmd.Parameters.Add( new DB2Parameter("@param1", 5));
+  cmd.Parameters.Add( new DB2Parameter("@param2", 6));
+  // Call the stored procedure
+  Console.WriteLine("Call stored procedure named " + procName);
+  DB2DataReader myReader = cmd.ExecuteReader();
+}
 
-// Register input-output and output parameters for the DB2Command
-cmd.Parameters.Add( new DB2Parameter("@param1", 5));
-cmd.Parameters.Add( new DB2Parameter("@param2", 6));
+//*******************************************************************
 
-// Call the stored procedure
-Console.WriteLine("Call stored procedure named " + procName);
-DB2DataReader myReader = cmd.ExecuteReader();
+
+//***************************** RUN METHODS HERE *****************************
+//Method to set up threads that run several stored procedures
+
+//****************************************************************************
+
+
+
+
+
+
+
 
 // always call Close when done reading.
 myReader.Close();
@@ -38,14 +58,10 @@ myReader.Close();
 conn.Close();
 Console.WriteLine("Connection Closed");  
 
-//****************************************************************************
 
-//Method to run stored procedure
-void startSelect() {
-  //program to run a stored procedure
-}
 
-//Method to set up threads that run several stored procedures
+
+
 
 
 
