2da4adee32d8b2281b92bee233b68834250e8503
patrickyuibm <150204185+patrickyuibm@users.noreply.github.com>
2023-11-10 21:32:57 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-//***************************** OPEN THE CONNECTION *****************************
+//***************************** IMPORT DEPENDENCIES *****************************
 using IBM.Data.Db2;
-
-string uid = Environment.GetEnvironmentVariable("uid");
-string pwd = Environment.GetEnvironmentVariable("pwd");
-string server = Environment.GetEnvironmentVariable("server");
-string db = Environment.GetEnvironmentVariable("db");
-
-//Connection String
-string connString = "uid=" + uid + ";pwd=" + pwd + ";server=" + server + ";database=" + db;
-DB2Connection conn = new DB2Connection(connString);
-conn.Open();
-Console.WriteLine("Connection Opened successfully");
+using System.Threading;
 //*******************************************************************************
 
 
@@ -22,10 +12,30 @@ Console.WriteLine("Connection Opened successfully");
 Note: currently, since the parameters for each SP are different, one method to run any SP is nearly impossible. We have to know what 
 SP we want to run the code it in first. 
 */
-void runSP(String spname = 'DB2ADM.INSERT_AND_SELECT_TB2', DB2Connection conn) {
+void main() {
+  string uid = Environment.GetEnvironmentVariable("uid");
+  string pwd = Environment.GetEnvironmentVariable("pwd");
+  string server = Environment.GetEnvironmentVariable("server");
+  string db = Environment.GetEnvironmentVariable("db");
+
+  //Connection String
+  string connString = "uid=" + uid + ";pwd=" + pwd + ";server=" + server + ";database=" + db;
+  DB2Connection conn = new DB2Connection(connString);
+  conn.Open();
+  Console.WriteLine("Connection Opened successfully");
+
+  //Establish multithreading
+  Thread[] myThreads = new Thread[numInsertThreads];
+  for (int i = 0; i < numInsertThreads; i++) {
+    myThreads[i] = new Thread(new ThreadStart(() => startSelect(conn)));
+  }
+}
+
+void run_insert_and_select_tb2_SP(DB2Connection conn) {
   //Set up a stored procedure
   DB2Transaction trans = conn.BeginTransaction();
   DB2Command cmd = conn.CreateCommand();
+  String spname = 'DB2ADM.INSERT_AND_SELECT_TB2'
   String procCall = "CALL " + spname + " (@param1, @param2)";
   cmd.Transaction = trans;
   cmd.CommandText = procCall;
@@ -37,12 +47,19 @@ void runSP(String spname = 'DB2ADM.INSERT_AND_SELECT_TB2', DB2Connection conn) {
   DB2DataReader myReader = cmd.ExecuteReader();
 }
 
+void startselect(DB2Connection conn) {
+  //call running stored procedures here
+  run_insert_and_select_tb2_SP(conn)
+}
+
+
+
 //*******************************************************************
 
 
 //***************************** RUN METHODS HERE *****************************
 //Method to set up threads that run several stored procedures
-
+main();
 //****************************************************************************
 
 
