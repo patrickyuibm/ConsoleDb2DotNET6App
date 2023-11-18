1393cdba5cb35822d8e798faf7c87f4c9395ab6c
patrickyuibm <150204185+patrickyuibm@users.noreply.github.com>
2023-11-17 17:45:10 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -51,7 +51,7 @@ void startSelect(String connectionString) {
   connb.Pooling = true;
   connb.MinPoolSize = 0;
   connb.MaxPoolSize = 10000;
-  Console.WriteLine('cb string = ' + connb.ConnectionString);
+  Console.WriteLine("cb string = " + connb.ConnectionString);
   DB2Connection conn = new DB2Connection(connb.ConnectionString);
   String thname = System.Threading.Thread.CurrentThread.Name;
   conn.Open();
