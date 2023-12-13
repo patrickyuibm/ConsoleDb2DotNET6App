a5955cd78db7726a8ce0f0befd6c0b2ec9fa97dc
patrickyuibm <150204185+patrickyuibm@users.noreply.github.com>
2023-12-12 17:35:19 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -98,7 +98,8 @@ void startSelect() {
   //SSL
   connb.SSLClientKeystash = "C:\\Program Files\\ibm\\gsk8\\bin\\zosclientdb.sth";
   connb.SSLClientKeystoredb = "C:\\Program Files\\ibm\\gsk8\\bin\\zosclientdb.kdb";
-  connb.SSLClientLabel = "label";
+  connb.SSLClientLabel = "clientcert";
+  connb.protocol = "tcpip";
   connb.Security = "SSL";
   connb.Authentication = "Certificate";
   //Logs errors
