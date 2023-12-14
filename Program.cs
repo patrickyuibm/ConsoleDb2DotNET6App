bbcc35d0f9434417e95cbd32bedbf300d3941f0a
patrickyuibm <150204185+patrickyuibm@users.noreply.github.com>
2023-12-14 09:21:55 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -182,14 +182,14 @@ void startSelect() {
 
 void testConnection() {
   String s = "DATABASE=DSNR3;SERVER=9.30.179.1:51600;"
-  s += "Userid=LGB0266";";
+  s += "Userid=LGB0266;";
   s += "Password=pilsner;";
   s += "Pooling=true;";
   s += "Max Pool Size=50;";
   s += "Min Pool Size=10;";
   s += "Connection Lifetime=60;";
   s += "Security=SSL;";
-  s += "SSLClientKeystoredb=zosclientdb.kdb";
+  s += "SSLClientKeystoredb=zosclientdb.kdb;";
   s += "SSLClientKeystash=zosclientdb.sth;";
   s += "AUTHENTICATION=CERTIFICATE;";
   s += "SSLCLIENTKEYSTOREDBPASSWORD=PASS;";
