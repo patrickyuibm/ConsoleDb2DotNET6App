0063f391ec1d1aab6309178b29bc783010c71e59
patrickyuibm <150204185+patrickyuibm@users.noreply.github.com>
2023-12-15 17:10:07 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -191,7 +191,7 @@ void testConnection() {
   //s += "Connection Lifetime=60;";
   s += "Security=SSL;";
   s += "AUTHENTICATION=CERTIFICATE;";
-  s += "SSLClientKeystoreDBP=/app/zosclientdb.kdb";
+  s += "SSLClientKeystoreDBP=/etc/keystore";
   s += "SSLCLIENTKEYSTOREDBPASSWORD=PASS;";
   s += "SSLCLIENTLABEL=clientcert;";
 
