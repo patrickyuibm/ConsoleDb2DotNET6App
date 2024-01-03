a81b25fcc8214742d5984af693c0313389f3bc2b
patrickyuibm <150204185+patrickyuibm@users.noreply.github.com>
2024-01-02 16:38:50 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -97,7 +97,7 @@ void startSelect() {
   try { 
     Stopwatch s = new Stopwatch();
     s.Start();
-    while (s.Elapsed < TimeSpan.FromSeconds(3600)) 
+    while (s.Elapsed < TimeSpan.FromSeconds(1800)) 
     {
         run_select_queries(conn);
     }
