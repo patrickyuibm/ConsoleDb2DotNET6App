ce5175facf617cce3d16abd46a0d6754cd5f1d97
patrickyuibm <150204185+patrickyuibm@users.noreply.github.com>
2024-01-04 11:02:42 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -134,12 +134,17 @@ void testConnection() {
   Console.WriteLine("Type pwd: ");
   String pwd = Console.ReadLine();
   connb.Password = pwd;
-  Console.WriteLine("Type SSL Server:Port: ");
+  Console.WriteLine("Type Server:Port: ");
   String sslserver = Console.ReadLine();
   connb.Server = sslserver;
-  connb.Security = "SSL";
-  connb.SSLClientKeystash = "/etc/stash/zosclientdb.sth";
-  connb.SSLClientKeystoredb = "/etc/keystore/zosclientdb.kdb";
+  bool ssl; 
+  Console.WriteLine("SSL? (True/False): ");
+  ssl = bool.Parse(Console.ReadLine());
+  if (ssl) {
+    connb.Security = "SSL";
+    connb.SSLClientKeystash = "/etc/stash/zosclientdb.sth";
+    connb.SSLClientKeystoredb = "/etc/keystore/zosclientdb.kdb";
+  }
   connb.Pooling = true;
   connb.MinPoolSize = 0;
   connb.MaxPoolSize = 10000;
