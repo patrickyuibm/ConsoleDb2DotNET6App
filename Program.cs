f98206c397bd391af4872eb6d0fb263fdd292e89
patrickyuibm <150204185+patrickyuibm@users.noreply.github.com>
2024-01-11 10:27:12 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -95,6 +95,7 @@ void main() {
 void startSelect() {
   int thid = System.Threading.Thread.CurrentThread.ManagedThreadId;
   DB2Connection conn = new DB2Connection();
+  conn.Open();
   conn.ConnectionString = "database=DSNR3;uid=db2adm;pwd=fantom;server=9.30.179.1:51600;clientuserid=patrick";
   try {
     DB2Command cmd1 = new DB2Command("INSERT INTO SYSIBM.DSN_PROFILE_TABLE (CLIENT_USERID, PROFILEID, PROFILE_ENABLED) VALUES ('Patrick', 20240110, 'Y')", conn);
