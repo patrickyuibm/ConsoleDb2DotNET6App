784b5f2a66709d9c2a45cda58515b28519b22c30
patrickyuibm <150204185+patrickyuibm@users.noreply.github.com>
2024-02-26 17:28:34 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -66,6 +66,7 @@ int connLT = 0;
 //***************************** METHODS *****************************
 
 void main() {
+  getConnectionProperties();
   Console.WriteLine("Insert number of threads: ");
   numInsertThreads = int.Parse(Console.ReadLine());
   Console.WriteLine("Enable SSL? (True/False): ");
@@ -156,6 +157,26 @@ void startSelect() {
   } finally { 
       conn.Close();
   }
+}
+
+void getConnectionProperties() {
+  Dictionary<string, string> props = new Dictionary<string, string>();
+  try {
+    // Create an instance of StreamReader to read from a file.
+    // The using statement also closes the StreamReader.
+    using (StreamReader sr = new StreamReader("/etc/Properties.txt")) {
+        String line;
+        // Read and display lines from the file until the end of
+        // the file is reached.
+        while ((line = sr.ReadLine()) != null) {
+          Console.WriteLine(line);
+        }
+      }
+  } catch (Exception e) {
+      // Let the user know what went wrong.
+      Console.WriteLine("The file could not be read:");
+      Console.WriteLine(e.Message);
+    }
 }
 
 void testConnection() {
